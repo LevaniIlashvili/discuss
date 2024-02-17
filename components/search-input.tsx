af9fc7b65851f16e6e0a51120890face4802e9a1
LevaniIlashvili <levaniko900@gmail.com>
2024-02-17 12:56:38 +0400

    initial commit

---
+++ components/search-input.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Input } from "@nextui-org/react";
+import { useSearchParams } from "next/navigation";
+import * as actions from "@/actions";
+
+export default function SearchInput() {
+  const searchParams = useSearchParams();
+
+  return (
+    <form action={actions.search}>
+      <Input
+        size="sm"
+        name="term"
+        defaultValue={searchParams.get("term") || ""}
+      />
+    </form>
+  );
+}
---
+++ db/queries/comments.ts
@@ -0,0 +1,19 @@
+import { Comment } from "@prisma/client";
+import { cache } from "react";
+import { db } from "..";
+
+export type CommentWithAuthor = Comment & {
+  user: { name: string | null; image: string | null };
+};
+
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("making query");
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: { select: { name: true, image: true } },
+      },
+    });
+  }
+);
